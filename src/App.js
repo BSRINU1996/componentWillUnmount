e7d58bf8b6e1e0198a2dd19f276abb1321b235b6
MeSrinu <balisrinu1996@gmail.com>
2022-12-20 15:19:38 +0530

    this is regarding the lifecycle emthod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component, Fragment } from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state =
+    {
+      name: "srinu",
+      id: 1234,
+      show: true,
+    }
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>componentWill Unmount</h1>
+        {this.state.show ? <Child /> : null}
+        <button onClick={() => { this.setState({ show: !this.state.show }) }} >toggle Child Component</button>
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
+
+
+
+export class Child extends Component {
+
+  componentWillUnmount() {
+    console.log("child component is hidden now!");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2>This is Child Component</h2>
+      </Fragment>
+    )
+  }
+}
